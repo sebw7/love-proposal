d24bbabe97bdd4adf8df3cca2aefcc605e1a1c5a
sebw7 <siamsheak8@gmail.com>
2026-07-04 17:23:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -60,3 +60,37 @@ document.getElementById("story").style.display="none";
 document.getElementById("letter").style.display="flex";
 
 });
+
+document.getElementById("proposalBtn").addEventListener("click",()=>{
+
+document.getElementById("letter").style.display="none";
+
+document.getElementById("proposal").style.display="flex";
+
+});
+
+function accepted(){
+
+document.body.innerHTML=`
+
+<div style="display:flex;justify-content:center;align-items:center;height:100vh;background:#050816;color:white;text-align:center;padding:20px;">
+
+<div>
+
+<h1 style="font-size:70px;">🎉❤️</h1>
+
+<h2>You Made Me The Happiest Person Alive!</h2>
+
+<h3>I Promise To Always Respect You, Care For You & Stay By Your Side. 💖</h3>
+
+</div>
+
+</div>
+
+`;
+
+}
+
+document.getElementById("yesBtn").onclick=accepted;
+
+document.getElementById("yesBtn2").onclick=accepted;

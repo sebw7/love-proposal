27f81a1c77d7d669685386823357f15e46637f6d
sebw7 <siamsheak8@gmail.com>
2026-07-04 19:00:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -153,3 +153,29 @@ setInterval(()=>{
     },3000);
 
 },1800);
+
+// Falling Rose Petals
+
+const petals=document.querySelector(".petals");
+
+setInterval(()=>{
+
+const petal=document.createElement("div");
+
+petal.className="petal";
+
+petal.innerHTML="🌹";
+
+petal.style.left=Math.random()*100+"vw";
+
+petal.style.animationDuration=(5+Math.random()*4)+"s";
+
+petals.appendChild(petal);
+
+setTimeout(()=>{
+
+petal.remove();
+
+},9000);
+
+},700);
